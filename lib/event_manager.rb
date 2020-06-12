66f5dbf1676faddbe05fbb0307405582c9f46cf8
Zach Baird <zdb1994@yahoo.com>
2020-06-12 10:19:19 -0500

    Removing old output files. Cleaned up logic. Bringing in phone numbers after cleaning them.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -13,6 +13,24 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+# Method to clean up bad phone numbers
+def clean_phone(phone)
+  phone = phone.to_s.split(Regexp.union(['.', ' ', '-']))
+  phone[0].gsub!('(', '')
+  phone[0].gsub!(')', '')
+  phone = "#{phone[0]}#{phone[1]}#{phone[2]}"
+
+  if phone.length < 10 || phone.length > 11 || (phone.length == 11 && phone[0] == 1)
+    phone = 'Invalid Phone Number'
+  elsif phone.length == 11 && phone[0] == '1'
+    phone = phone[1..phone.length]
+  end
+
+  puts phone
+
+  phone
+end
+
 # Gets legistlators with the Google Civic Info API
 def legislators_by_zipcode(zip)
   # Assign API info for the following methods
@@ -36,27 +54,39 @@ def legislators_by_zipcode(zip)
   end
 end
 
+# Create output file
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist? 'output'
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 # Reads from the CSV file, pulls out the names and zipcodes, and then pulls
 #  the legislators for that zipcode to spin up a thank you HTML letter per person
 def read_first_names_and_zipcodes_then_use_api
   contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
   contents.each do |row|
+    id = row[0]
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
+
+    phone_number = row[:homephone]
+
+    phone = clean_phone(phone_number)
+
     legislators = legislators_by_zipcode(zipcode)
 
+    # Get output file spun up and bound with erb
     template_letter = File.read 'form_letter.erb'
     erb_template = ERB.new template_letter
     form_letter = erb_template.result(binding)
 
-    # output to a file
-    Dir.mkdir('output') unless Dir.exist? 'output'
-
-    filename = "output/thanks_#{name}-#{zipcode}.html"
-
-    File.open(filename, 'w') do |file|
-      file.puts form_letter
-    end
+    # Output to a file
+    save_thank_you_letter(id, form_letter)
   end
 end
 
